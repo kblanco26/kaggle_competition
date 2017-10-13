5119ba6b7c78602d51750aec4214157bcb3649f4
kblanco26 <krystal.blanco@asu.edu>
2017-10-12 18:33:36 -0700

    Adding Krystal's Code
    Adding R code

---
+++ titanic/code/titanic.R
@@ -0,0 +1,10 @@
+#################################################
+# titanic.R                                     #
+#                                               #
+# Description: Machine learning for Titanic     #
+# data set on Kaggle                            #
+#                                               #
+#                                               #
+#                                               #
+# Authors: The Angry Zombies                    #
+#################################################
