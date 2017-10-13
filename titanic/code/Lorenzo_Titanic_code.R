6e13d9917f6e8f16d001865b60c330429f68c3d5
jltapia <jltapia@asu.edu>
2017-10-12 20:15:44 -0700

    First draft of code

--- titanic/code/Lorenzo_Titanic_code.R
+++ titanic/code/Lorenzo_Titanic_code.R
@@ -1,5 +1,27 @@
 
-setwd("~/Documents/kaggle_competition/titanic/data")
+setwd("~/Documents/GitHub/kaggle_competition/titanic/data")
+installed.packages("tidyverse")
+library(tidyverse)
 
-#this is a test
+gender_submission <- read.csv(file="gender_submission.csv",stringsAsFactors = FALSE)
+test <- read.csv(file="test.csv", stringsAsFactors = FALSE)
+train <- read.csv(file="train.csv",stringsAsFactors = FALSE)
 
+train$Survived <- factor(train$Survived)
+
+train$Sex <- factor(train$Sex)
+train$Embarked <- factor(train$Embarked,levels=c("C","Q","S"),
+                        labels=c("Cherbourg","Queenstown","Southampton"))
+train$Pclass <- factor(train$Pclass)
+
+unique(train$Embarked)
+testing <- train[train$Embarked == "",]
+
+ ggplot(data=train, aes(x=Survived, fill=Embarked)) +
+    geom_bar()
+ 
+ ggplot(data=train, aes(fill=Pclass, x=Embarked)) +
+   geom_bar()
+ 
+
+ 
